a0b73e34c0e12bffcf465f1ebeb66f45691c0faf
terry_u16 <terry-u16@users.noreply.github.com>
2020-07-18 02:30:05 +0900

    Add EducationalCodeforcesRound091

---
+++ EducationalCodeforcesRound091/EducationalCodeforcesRound091/EducationalCodeforcesRound091/Questions/QuestionF.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using EducationalCodeforcesRound091.Extensions;
+using EducationalCodeforcesRound091.Questions;
+
+namespace EducationalCodeforcesRound091.Questions
+{
+    public class QuestionF : AtCoderQuestionBase
+    {
+        public override IEnumerable<object> Solve(TextReader inputStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
